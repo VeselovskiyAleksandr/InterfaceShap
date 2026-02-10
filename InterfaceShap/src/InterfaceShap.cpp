b2cc98d96527703f105057886825eb53e029c6b4
VeselovskiyAleksandr <VeselovskiyAleksandr@mail.ru>
2026-02-10 20:09:15 +0300

    InterfaceShap

--- InterfaceShap/src/InterfaceShap.cpp
+++ InterfaceShap/src/InterfaceShap.cpp
@@ -15,9 +15,9 @@ int main()
 	do {
 		cout << "\nSpecify the radius of the circle.";
 		cin >> firstPar;
-		if (firstPar < 0)
+		if (firstPar <= 0)
 			cout << "\nThe radius of the circle is indicated incorrectly.";
-	} while (firstPar < 0);
+	} while (firstPar <= 0);
 	circle.setParametr(firstPar, 0, 0);
 	printParams(&circle);
 	Rectangle rectangle;
@@ -26,9 +26,9 @@ int main()
 		cin >> firstPar;
 		cout << "\nSpecify the width of the rectangle.";
 		cin >> secondPar;
-		if (firstPar < 0 || secondPar < 0)
+		if (firstPar <= 0 || secondPar <= 0)
 			cout << "\nThe sides of the rectangle are indicated incorrectly.";
-	} while (firstPar < 0 || secondPar < 0);
+	} while (firstPar <= 0 || secondPar <= 0);
 	rectangle.setParametr(firstPar, secondPar, 0);
 	printParams(&rectangle);
 	Triangle triangle;
@@ -39,11 +39,11 @@ int main()
 		cin >> secondPar;
 		cout << "\nSpecify the third side of the triangle.";
 		cin >> thirdPar;
-		if (firstPar < 0 || secondPar < 0 || thirdPar < 0)
+		if (firstPar <= 0 || secondPar <= 0 || thirdPar <= 0)
 			cout << "\nThe sides of the triangle are indicated incorrectly.";
 		else if (((firstPar + secondPar)<= thirdPar) || ((firstPar + thirdPar) <= secondPar) || ((secondPar + thirdPar) <= firstPar))
 			cout << "\nThere is no triangle with the given sides.";
-	} while ((firstPar < 0 || secondPar < 0 || thirdPar < 0) || ((firstPar + secondPar) <= thirdPar) || ((firstPar + thirdPar) <= secondPar) || ((secondPar + thirdPar) <= firstPar));
+	} while ((firstPar <= 0 || secondPar <= 0 || thirdPar <= 0) || ((firstPar + secondPar) <= thirdPar) || ((firstPar + thirdPar) <= secondPar) || ((secondPar + thirdPar) <= firstPar));
 	triangle.setParametr(firstPar, secondPar, thirdPar);
 	printParams(&triangle);
 	return 0;
